6932a350b69daef4d44182f823250e205671fce6
Divya Sharma <divyaonline20@gmail.com>
2023-04-02 09:19:02 +0530

    font

--- reclaim2/src/components/SellCard.js
+++ reclaim2/src/components/SellCard.js
@@ -1,19 +1,24 @@
 export default function SellCard() {
-    return (
-      <div class="max-w-sm rounded overflow-hidden shadow-lg">
-        <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-          <p class="text-gray-700 text-base">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
-            quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
-            nihil.
-          </p>
-        </div>
-        <div class="px-6 pt-4 pb-2">
-          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-lg font-semibold text-green-600  hover:bg-green-600 hover:text-white mr-2 mb-2">
-            Buy
-          </span>
-        </div>
+  return (
+    <div class="max-w-sm rounded overflow-hidden shadow-lg over">
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+        <img
+          src="https://www.adani.com/-/media/Project/LucknowAirport/Traveller/AirportServices/LostAndFound/lost-and-found.png"
+          alt="lmao"
+          class="w-100%"
+        />
+        <p class="text-gray-700 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+          quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
+          nihil.
+        </p>
       </div>
-    );
+      <div class="px-6 pt-4 pb-2">
+        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-lg font-semibold text-green-600  hover:bg-green-600 hover:text-white mr-2 mb-2">
+          Buy
+        </span>
+      </div>
+    </div>
+  );
 }
--- reclaim2/src/pages/Sell.js
+++ reclaim2/src/pages/Sell.js
@@ -3,7 +3,7 @@ import Footer from "../components/Footer";
 export default function Sell() {
   return (
     <div className="text-4xl font-medium">
-      <div className="p-10 dark:bg-gray-900 text-white">Buy and Sell</div>
+      {/* <div className="p-10 dark:bg-gray-900 text-white">Buy and Sell</div> */}
       <div className="p-10">
         <div className="text-3xl p-4">Shop or sell</div>
         <SellCard />
@@ -12,30 +12,36 @@ export default function Sell() {
         <div className="flex flex-col p-10 rounded-lg">
           <h1 className="text-4xl mb-0 m-10">Want to sell something?</h1>
           <p className="text-lg ml-10">Sell your belongings here!!</p>
-          <form>
-            <label className="m-10 text-sm mb-0 rounded-md" htmlFor="">
-              item name
-            </label>
-            <input
-              type="text"
-              className="border-solid border-black block m-10"
-              placeholder="hello"
-            />
+          <form className="flex flex-col justify-center">
+            <div className="flex">
+              <>
+                <label className="m-10 text-sm mb-0 rounded-md" htmlFor="">
+                  item name
+                </label>
+                <input
+                  type="text"
+                  className="border-solid border-black block m-10"
+                  placeholder="Item-Name"
+                />
+              </>
+              <>
+                <label className="m-10 text-sm mb-0 rounded-md" htmlFor="">
+                  Price
+                </label>
+                <input
+                  type="text"
+                  className="border-solid border-black block m-10"
+                  placeholder="Price Offered"
+                />
+              </>
+            </div>
             <label className="m-10 text-sm mb-0 rounded-md" htmlFor="">
               Description
             </label>
             <input
-              type="text"
+              type="text-area"
               className="border-solid border-black block m-10"
-              placeholder="hello"
-            />
-            <label className="m-10 text-sm mb-0 rounded-md" htmlFor="">
-              Price
-            </label>
-            <input
-              type="text"
-              className="border-solid border-black block m-10"
-              placeholder="Price Offered"
+              placeholder="Description"
             />
             <button
               type="Submit"
